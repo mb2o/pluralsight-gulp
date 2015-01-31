7f3ae4d3b22be71d5d9d560f4801197e35aae986
mb2o <mboomaars@gmail.com>
2015-01-31 14:41:55 +0100

    less to css conversion

--- gulp.config.js
+++ gulp.config.js
@@ -1,11 +1,16 @@
 module.exports = function () {
+    var client = './src/client/';
+
     var config = {
+        temp: './.tmp/',
 
         // all js files to vet
         alljs: [
             './src/**/*.js',
             './*.js'
-        ]
+        ],
+
+        less: client + 'styles/styles.less'
     };
 
     return config;
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 var gulp = require('gulp');
 var args = require('yargs').argv;
+var del = require('del');
+
 var config = require('./gulp.config')();
 
-var $ = require('gulp-load-plugins')({lazy: true});
+var $ = require('gulp-load-plugins')({
+    lazy: true
+});
 
 gulp.task('vet', function () {
     'use strict';
@@ -13,11 +17,51 @@ gulp.task('vet', function () {
         .pipe($.if(args.verbose, $.print()))
         .pipe($.jscs())
         .pipe($.jshint())
-        .pipe($.jshint.reporter('jshint-stylish', {verbose: true}))
+        .pipe($.jshint.reporter('jshint-stylish', {
+            verbose: true
+        }))
         .pipe($.jshint.reporter('fail'));
 });
 
+gulp.task('styles', ['clean-styles'], function () {
+    log('Compiling Less --> CSS');
+
+    return gulp
+        .src(config.less)
+        .pipe($.plumber())
+        .pipe($.less())
+        .pipe($.autoprefixer({
+            browsers: ['last 2 version', '> 5%']
+        }))
+        .pipe(gulp.dest(config.temp));
+});
+
+// Does not have a stream, so should use a callback
+gulp.task('clean-styles', function (done) {
+    var files = config.temp + '**/*.css';
+
+    clean(files, done);
+});
+
+gulp.task('less-watcher', function () {
+    gulp.watch([config.less], ['styles']);
+});
+
 /////////////////////
+
+//function errorLogger(error) {
+//    log('*** Start of Error ***');
+//    log(error);
+//    log('*** End of Error ***');
+//
+//    // Use Gulps' emit to end the pipeline!
+//    this.emit('end');
+//}
+
+function clean(path, done) {
+    log('Cleaning: ' + $.util.colors.blue(path));
+    del(path, done);
+}
 
 function log(msg) {
     'use strict';
